a657890b751cebc415cedf21ebf6b39a9d6ffe0f
Drihan du Preez <drihan@btinternet.com>
2022-10-10 14:57:13 +0100

    program will now guess a number between the 2 bounds and ask if it is higher or lower

--- NumberGuessingGame.py
+++ NumberGuessingGame.py
@@ -10,3 +10,12 @@ if lower_bound < 0:
             break
 print("Please enter the upper bound number")
 upper_bound = int(input(">"))
+print(f"Think of a number between {lower_bound} and {upper_bound}.")
+guess = (upper_bound - lower_bound) // 2
+print(f"Is your number {guess}. Please enter 'Yes' or 'No.")
+answer = input(">")
+if answer == 'yes'.casefold():
+    print("I guessed it!")
+elif answer == 'no'.casefold():
+    print("Is your number higher or lower?")
+    higherorlower = input(">")
