aac328bb4e9a2f504ce641f8dcc5439a753b3349
Drihan du Preez <drihan@btinternet.com>
2022-10-10 13:54:04 +0100

    initial lower and upper bounds can be set

--- NumberGuessingGame.py
+++ NumberGuessingGame.py
@@ -1,1 +1,12 @@
 # Number guessing Game
+
+print("Please enter the lower bound number (cannot be less than 0)")
+lower_bound = int(input(">"))
+if lower_bound < 0:
+    while lower_bound < 0:
+        print("Value entered is lower than 0, please enter a value that is 0 or higher")
+        lower_bound = int(input(">"))
+        if lower_bound >= 0: 
+            break
+print("Please enter the upper bound number")
+upper_bound = int(input(">"))
